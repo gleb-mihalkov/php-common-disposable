1915f6c8582e5754160062cbfcc33c1e69d7f811
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-09-16 00:14:35 +0300

    Обновление документации.

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Common\\IDisposable"],["c","Common\\TDisposable"]];
+ApiGen.elements = [["c","Common\\DisposableDisposedException"],["c","Common\\DisposableException"],["c","Common\\IDisposable"],["c","Common\\TDisposable"]];
